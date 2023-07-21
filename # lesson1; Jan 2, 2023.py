ad5845331eb49dc556b6c0073dfe7e3d05f00235
gian_ganca@hotmail.com <gian_ganca@hotmail.com>
2023-07-21 04:22:39 +0200

    lesson1 + git commands

---
+++ # lesson1; Jan 2, 2023.py
@@ -0,0 +1,42 @@
+# Terminal commandos
+# lesson1; Jan 2, 2023
+
+# ls= list; shows maps and files on current position
+# ls -la= list all formats
+# cd= change directory: moves through maps and files
+# cd = only typing cd takes you back to ROOT (main), where terminal always starts from (\users\dell\)
+# cd . = Note: cd(space). :cd_. = move to this position
+# cd .. = Move one step up (move backwards)
+# clear =rensa terminal
+# python .\... = run in python in current position
+# python .\test.py = runs program;file
+# python .\ds\python\python101\lessons\lesson1 = runs in python from current position (you´ve to specify long way position)
+# mkdir :make directory = makes a map
+# rm :remove = removes a file
+# rmdir :remove directory = removes/delete directory (have to be empty)
+# rm -r :remove recusive = removes all from directory
+# rm -rf: remove recusive force (not to use without positions, can remove all Operative system) remove everything
+# touch: make on unix(bash,linux) = skapa fil
+# ni : new item = make a new file on powershell
+# .py = make file with python format
+# .md :mark down= make file with text format
+# mv : move = move a file uses: mv filepath(input)+space+new position(output)
+# cp :copy = copies a file, map uses: cp file+space+new position (works to change name and copy same file)
+# cp file ..\map\file = you can change map and copy file
+# nano file = opens a simpel texteditor on 
+# cat = shows, reads a text file
+
+# Git commands
+# git -- version = show the version of git
+# git init: git initialize = creates, initializes git empty repository in current map
+# git push = save current proccess to git, push up the code to the repo
+# git status = show the tracked/untracked files on git
+# git add . = add proccess to the repository
+# git commit -m "Initial commit" : -m= message = git commit: send to git because the file is ready(done, nothing more to do with, works as a save file in the repo) 
+# git --help = show help commands
+# git log = shows all commits, (also displays username and email logged in)
+# git remote add origin https...= remote push to origin repo
+# branch = working tree (tree branch) in the code, github project
+# git branch -M main: create a branch in MASTER called "main"
+# git push -u origin main = push up to upstream (which remote) - origin to branch main (send to github)
+# video: 1:32:28
---
+++ new_map/test.py
@@ -0,0 +1,2 @@
+# lesson1; Jan 2, 2023
+print ("hello world")
